ff8feb38c8bdb1a8284535828410115267a96839
ian_mcd <ian_mcdonald@rocketmail.com>
2025-10-15 22:22:26 -0700

    no static ip config

--- nix/hosts/busy-bee/configuration.nix
+++ nix/hosts/busy-bee/configuration.nix
@@ -34,20 +34,20 @@ in {
       #   };
       # };
     };
-    interfaces.enp1s0 = {
-      ipv4.addresses = [
-        {
-          address = "192.168.0.104";
-          prefixLength = 24;
-        }
-      ];
-    };
-    defaultGateway = "192.168.0.1";
-    nameservers = [
-      "192.168.0.1"
-      "75.75.75.75"
-    ];
-  };  
+  #   interfaces.enp1s0 = {
+  #     ipv4.addresses = [
+  #       {
+  #         address = "192.168.0.104";
+  #         prefixLength = 24;
+  #       }
+  #     ];
+  #   };
+  #   defaultGateway = "192.168.0.1";
+  #   nameservers = [
+  #     "192.168.0.1"
+  #     "75.75.75.75"
+  #   ];
+  # };  
 
   # services.zerotierone = {
   # enable = true;
