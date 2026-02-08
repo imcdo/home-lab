10aa23ab1003323307125ba080723df593263250
ian_mcd <ian_mcdonald@rocketmail.com>
2026-02-08 11:38:52 -0800

    revert

--- nix/modules/vintage-story-server.nix
+++ nix/modules/vintage-story-server.nix
@@ -8,90 +8,80 @@ let
   group = "vintage-story";
   homeDir = "/home/${user}";
   dataDir = "${homeDir}/data";
-  serverConfigFile = pkgs.writeText "serverconfig.json" (builtins.toJSON (recursiveUpdate {
-      ServerName = "NixOS Vintage World";
-      Port = cfg.port;
-      Password = cfg.password;
-      VerifyPlayerAuth = true;
-      # Required to point to the internal data folder
-      DataPath = dataDir; 
-    } cfg.extraSettings));
+  christianSshPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCgGX3jzujNs6a192SuIC75KUOsyGfTN6elM2CXtcuimnqxOOa19Ect6RMb9OVdNi4BkzIHvYrES9WJDFqYpaDzpX6yYmBeg47aKps+n16+Y1PPqU9DkJDNBbqXHb3YsHFX6jq+Dc7ledUy64hyrQuhID/jajSC7ZSOiFLfzpX7yjWMXjgciyIfDgmi68ZAyzHUODN1/Ab5fV6HLTiNSJbTzMoVyvb9f86uCTdbCYEEk0pLCoRZoUaBMD+hvXu0NM8nclXT1bWe7nVSijaLeBOLAG8SGEun7LxN7jbVFmHtUDg/rT33ACmZHVHLNu6P47oJ4YyILuXzK7wWCZVb7vU4lP9HBbfgWCNRtiNokGzyi2Y5amGWqWvxPEKSRTXSTXie18XyjehFkLuKCjvLOykYGSQA7NM3mEDqBeiaKyB9Sl4kF9gEOWZ24mHQqIxbMFWY60IdnPqpF1KLy1oVg0KnxmC2LCbd4GSMm2vzgEPNM+F/nfVW4CcnLqiI1AmW3q9GX4BYDX9KcRYaqrzA2sNGlvCAnpr6XVP2OBBcTJCHCs4S3unUMiRlN7m1xWgAP2DNqjy5MObgau8JDjvV8Xcv7fLwDTKxPJTzZGGPazQq3brIbXGKhkQNXdghVe7Ld8OV5uzyEUQQoUiYER2Hh5ATukNkM3qvpAtjaGZcsHPt9Q== christian@grandlan.dev";
+  ianSshPublicKey ="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJwCoP+9JDU6mH4pZCsk/GlhDXiarbdyaakIB1DzLMRtiv79U/aTkTvgm/TTmeQLM0W3vHYsKDloNRhRK87UbN798aiYk1g6w51OL7ClxlGStpZoRtTAA+enG2g55Vhx7WUM0kKvYw44iSWH60NN+XCItdHrGB6hBNf9Q86h+fzv2U92PvZOEjdX2PaNZ/2RR3QA6kf1ra8Na5RdXu3wvAZx+qAzrPXP8TGShcMc1kdYFC/RPzkUrj0Y2il3LXO7gAo1fi+RyZi9y0vvK3YNDHqxVE+dmMNYz9Ipsy2QBHF7vowJajvJVEAn8DQDSeQqRWwVeQZPTywzZbG8Ng0HlNV1QjUQbh3ZB3lWUdu5RQqD+Tltzo6fWkkN49FiYse/zlrIiUSayvALcGxeyvKTa0udIO2mGZO94aY/pg5uhG4/dHNk3JWRI2QyE0RyxCBRn9YksMPXVgkQ/ARgIbqrNP22JLFeffeB+zfBQQiPGsfnqTr8RWTyzlkltom6Uh5dksn7WfnbTofQbMIw6bU9x15+tmoxgJm3QzTnandpVXOsxSx5M2NJyTYIvkKegbJcRS0C4AiUeLDhm4feN/fg6oSRV4m+qpeFug0bO0AqjjKaaYOMHS6FoyT0osoLECMg0NjFdSuOVAdp7eB3sZD3nTtTPsnayyj+3uip+ajNhahw== ian@DESKTOP-C07E16P";
 
-    # 2. Generate the Users/Admins JSON
-    # This maps your Nix list of admins into the format Vintage Story expects.
-    usersFile = pkgs.writeText "users.json" (builtins.toJSON (map (admin: {
-      Uid = admin.uid;
-      Name = admin.name;
-      RoleCode = "admin";
-      # These are boilerplate requirements for the JSON schema
-      TenantId = "default";
-      AclState = 0;
-    }) cfg.admins));
+
 in {
   options.services.homelab.vintageStory = {
-    enable = mkEnableOption "Vintage Story Server";
+    enable = mkEnableOption "Enable Vintage Story Dedicated Server";
 
-    port = mkOption { type = types.port; default = 42420; };
-    password = mkOption { type = types.nullOr types.str; default = null; };
-
-    # The "Nix Way" Admin List
-    admins = mkOption {
-      type = types.listOf (types.submodule {
-        options = {
-          name = mkOption { type = types.str; description = "Player username"; };
-          uid = mkOption { type = types.str; description = "The player's Vintage Story UID"; };
-        };
-      });
-      default = [];
+    sshKeys = mkOption {
+      type = types.listOf types.str;
+      default = [ christianSshPublicKey ianSshPublicKey ]; 
+      description = "SSH public keys for server access";
     };
 
-    extraSettings = mkOption {
-      type = types.attrs;
-      default = {};
-      description = "Any other serverconfig.json keys you want to override";
+    port = mkOption {
+      type = types.port;
+      default = 42420;
+      description = "The port the server listens on";
     };
   };
 
   config = mkIf cfg.enable {
+    # 1. Allow Unfree (Vintage Story is not open source)
+    # Note: You may need this in your global configuration.nix as well
     nixpkgs.config.allowUnfree = true;
 
+    # 2. Networking
     networking.firewall.allowedTCPPorts = [ cfg.port ];
     networking.firewall.allowedUDPPorts = [ cfg.port ];
 
+    # 3. User Setup
     users.groups.${group} = {};
     users.users.${user} = {
       group = group;
       isSystemUser = true;
+      description = "Vintage Story Server User";
+      shell = pkgs.bash;
       home = homeDir;
       createHome = true;
-      shell = pkgs.bash;
+      packages = with pkgs; [
+        pkgs-unstable.vintagestory # The unstable package for latest features
+        screen
+        tmux
+      ];
+      openssh.authorizedKeys.keys = cfg.sshKeys;
     };
 
+    # 4. The Systemd Service
     systemd.services.vintage-story = {
       description = "Vintage Story Dedicated Server";
-      wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
+      wants = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
         User = user;
         Group = group;
         WorkingDirectory = homeDir;
-        # Use the binary from the Nix package
+
+        # We use the binary provided by the unstable Nix package.
+        # The --dataPath flag ensures your saves stay persistent in /home/vintage-story/data
         ExecStart = "${pkgs-unstable.vintagestory}/bin/vintagestory-server --dataPath ${dataDir}";
+
         Restart = "always";
+        RestartSec = "10s";
+
+        # Standard memory/cpu limits (matching your Satisfactory style)
+        MemoryAccounting = true;
+        CPUAccounting = true;
       };
 
       preStart = ''
         mkdir -p ${dataDir}
-        
-        # Copy the Nix-generated configs into the data directory
-        # We use 'cp' instead of 'ln' because the server occasionally 
-        # likes to write updates to these files.
-        cp -f ${serverConfigFile} ${dataDir}/serverconfig.json
-        cp -f ${usersFile} ${dataDir}/users.json
-        
         chown -R ${user}:${group} ${dataDir}
-        chmod 600 ${dataDir}/*.json
       '';
     };
   };
