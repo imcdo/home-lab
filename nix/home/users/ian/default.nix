c97ee59941e7d9284e75d258ac6e64f017dfe4f3
ian_mcd <ian_mcdonald@rocketmail.com>
2026-03-31 14:43:48 -0700

    update user

--- nix/home/users/ian/default.nix
+++ nix/home/users/ian/default.nix
@@ -1,16 +1,67 @@
 { pkgs, lib, sshConfig, ... }:
 
 {
-  home.username = "ian";
-  home.homeDirectory = "/home/ian";
-  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
+
+  user = {
+    full_name = "Ian McDonald";
+    username = "ian";
+  };
+
+  home = {
+    username = "ian";
+    homeDirectory = "/home/ian";
+    stateVersion = "23.11";
+
+    file.".ssh/authorized_keys" = {
+      text = ''
+        ${sshConfig.defaultPublicKey}
+      '';
+    };
+
+    packages = with pkgs; [
+      ripgrep
+      fd
+      tmux
+      bat
+      neovim
+      git
+      curl
+      wget
+      jq
+      tree
+      k9s
+      kubectl
+      helm
+      docker
+      wget
+      fluxcd
+      python3
+      cloudflared
+      btop
+      iptables
+      etcd
+      unixtools.ping
+      unixtools.netstat
+      python313Packages.uptime
+    ]  ++ (with pkgs.python313Packages; [
+      pip
+      virtualenv
+      pipx
+      requests
+    ]);
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
 
   # Basic shell configuration
   programs.zsh = {
     enable = true;
     shellAliases = {
       ll = "ls -la";
+      k = "kubectl";
       update = "sudo nixos-rebuild switch";
     };
     history.size = 10000000;
@@ -19,47 +70,17 @@
       enable = true;
       plugins = [
         "git"
+        "ssh"
+        "emoji"
+        "autojump"
+        "fluxcd"
+        "helm"
+        "kubectl"
+        "kubectx"
+        "pip"
+    
       ];
       theme = "simple";
     };
-  };
-
-  # Install packages for your user
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    tmux
-    bat
-    neovim
-    git
-    curl
-    wget
-    jq
-    tree
-    k9s
-    kubectl
-    helm
-    docker
-    wget
-    fluxcd
-    python3
-    cloudflared
-    btop
-    iptables
-    etcd
-    unixtools.ping
-    unixtools.netstat
-    python313Packages.uptime
-  ]  ++ (with pkgs.python313Packages; [
-    pip
-    virtualenv
-    pipx
-    requests
-  ]);
-
-  home.file.".ssh/authorized_keys" = {
-    text = ''
-      ${sshConfig.defaultPublicKey}
-    '';
   };
 }
