0bc4b732206eb9ef8f9616b9d64297deffc6f2d8
ian_mcd <ian_mcdonald@rocketmail.com>
2026-09-13 22:46:23 -0700

    ssh

---
+++ nix/home/users/fungus/default.nix
@@ -0,0 +1,104 @@
+{
+  pkgs,
+  lib,
+  sshConfig,
+  homeDirectory ? "/home/fungus",
+  username ? "fungus",
+  ...
+}: let
+  fungusSshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzpXXTMvAHsjA462PYq+S8krCnvuauh5CIS0IHA+RcK fungus@fungus-pc";
+in {
+  programs.home-manager.enable = true;
+
+  home = {
+    username = username;
+    homeDirectory = homeDirectory;
+    stateVersion = "23.11";
+
+    file.".ssh/authorized_keys" = {
+      text = ''
+        ${fungusSshPublicKey}
+      '';
+    };
+
+    packages = with pkgs;
+      [
+        autojump
+        ripgrep
+        fd
+        tmux
+        bat
+        neovim
+        git
+        curl
+        wget
+        jq
+        tree
+        k9s
+        kubectl
+        fluxcd
+        python3
+        cloudflared
+        btop
+        etcd
+        screen
+        opencode
+        opencode-desktop
+        python313Packages.uptime
+        godot_4_6_1
+      ]
+      ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+        helm
+        docker
+        iptables
+        unixtools.ping
+        unixtools.netstat
+      ])
+      ++ lib.optionals pkgs.stdenv.isDarwin [
+        darwinRebuildWrapper
+      ]
+      ++ (with pkgs.python313Packages; [
+        uv
+        requests
+      ]);
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.git = {
+    enable = false;
+  };
+
+  # Basic shell configuration
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -la";
+      k = "kubectl";
+    };
+    history = {
+      size = 10000000;
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "ssh"
+        "emoji"
+        "autojump"
+        "fluxcd"
+        "helm"
+        "kubectl"
+        "kubectx"
+        "pip"
+      ];
+      theme = "simple";
+    };
+  };
+}
--- nix/modules/users.nix
+++ nix/modules/users.nix
@@ -7,6 +7,7 @@
 with lib; let
   cfg = config.services.homelab.users;
   christianSshPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCgGX3jzujNs6a192SuIC75KUOsyGfTN6elM2CXtcuimnqxOOa19Ect6RMb9OVdNi4BkzIHvYrES9WJDFqYpaDzpX6yYmBeg47aKps+n16+Y1PPqU9DkJDNBbqXHb3YsHFX6jq+Dc7ledUy64hyrQuhID/jajSC7ZSOiFLfzpX7yjWMXjgciyIfDgmi68ZAyzHUODN1/Ab5fV6HLTiNSJbTzMoVyvb9f86uCTdbCYEEk0pLCoRZoUaBMD+hvXu0NM8nclXT1bWe7nVSijaLeBOLAG8SGEun7LxN7jbVFmHtUDg/rT33ACmZHVHLNu6P47oJ4YyILuXzK7wWCZVb7vU4lP9HBbfgWCNRtiNokGzyi2Y5amGWqWvxPEKSRTXSTXie18XyjehFkLuKCjvLOykYGSQA7NM3mEDqBeiaKyB9Sl4kF9gEOWZ24mHQqIxbMFWY60IdnPqpF1KLy1oVg0KnxmC2LCbd4GSMm2vzgEPNM+F/nfVW4CcnLqiI1AmW3q9GX4BYDX9KcRYaqrzA2sNGlvCAnpr6XVP2OBBcTJCHCs4S3unUMiRlN7m1xWgAP2DNqjy5MObgau8JDjvV8Xcv7fLwDTKxPJTzZGGPazQq3brIbXGKhkQNXdghVe7Ld8OV5uzyEUQQoUiYER2Hh5ATukNkM3qvpAtjaGZcsHPt9Q== christian@grandlan.dev";
+  fungusSshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzpXXTMvAHsjA462PYq+S8krCnvuauh5CIS0IHA+RcK fungus@fungus-pc";
 in {
   options.services.homelab.users = {
     enable = mkEnableOption "Enable homelab user configuration";
@@ -116,6 +117,22 @@ in {
       home = "/home/christian";
       packages = cfg.mainUser.packages;
       openssh.authorizedKeys.keys = [christianSshPublicKey];
+    };
+
+    users.users.fungus = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "nix"
+        "nix-admins"
+        "k3s"
+        "docker"
+      ];
+      description = "Fungus";
+      shell = pkgs.bash;
+      home = "/home/fungus";
+      packages = cfg.mainUser.packages;
+      openssh.authorizedKeys.keys = [fungusSshPublicKey];
     };
 
     # Admin user
